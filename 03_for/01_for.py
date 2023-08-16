f95ae15eb9a5a107becde7ee110bddac7524720f
YuPhe1 <oksunboy1@naver.com>
2023-08-16 13:43:45 +0900

    for

---
+++ 03_for/01_for.py
@@ -0,0 +1,9 @@
+# range를 이용하여 순차적으로 숫자 출력
+
+for i in range(10):
+    print(i, end=" ")
+
+print()
+
+for i in range(5, 12, 2):
+    print(i, end=" ")
---
+++ 03_for/02_for.py
@@ -0,0 +1,15 @@
+list1 = [10, 20, 30, 40, 50]
+for i in list1:
+    print(i, end=" ")
+
+print()
+
+list2 = ["가", "나", "다", "라", "마"]
+for i in list2:
+    print(i, end=" ")
+
+print()
+
+list3 = ["python", "java", "c", ["javascript", "typescript"]]
+for i in list3:
+    print(i, end=" ")
