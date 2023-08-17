6342fc12127f4c92f9b5036773c6ea42f86a1dfb
YuPhe1 <oksunboy1@naver.com>
2023-08-17 14:35:05 +0900

    07_selenium

--- 07_selenium/ex02_selenium.py
+++ 07_selenium/ex02_selenium.py
@@ -7,10 +7,13 @@ driver = webdriver.Chrome()
 # 접속할 주소
 driver.get("https://www.example.com")
 
-# p 태그 요소만 접근하기
-driver.find_elements(By.TAG_NAME, 'p')
+# # p 태그 요소만 접근하기
+# p_element = driver.find_element(By.TAG_NAME, 'p') # 첫번째 p태그에만 접근 
+# print(p_element)
+# print(type(p_element))
+# print(p_element.text)
 
-
-# 10초 동안 현재 상태에서 대기
-time.sleep(10)
-
+p_elements = driver.find_elements(By.TAG_NAME, 'p')
+print(type(p_elements))
+for p in p_elements:
+    print(p.text)
