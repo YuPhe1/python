3e06429aa7702325e113cf7c7899b3e911bf8fbf
YuPhe1 <oksunboy1@naver.com>
2023-08-17 14:16:49 +0900

    07_selenium

--- 07_selenium/ex02_selenium.py
+++ 07_selenium/ex02_selenium.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 # 크롬 브라우저 실행
 driver = webdriver.Chrome()
 # 접속할 주소
-driver.get("https://www.google.com")
+driver.get("https://www.example.com")
+
+# p 태그 요소만 접근하기
+
 # 10초 동안 현재 상태에서 대기
-time.sleep(10) # 웹페이지가 다 열리기전에 종료되는 것을 방지
+time.sleep(10)
+
