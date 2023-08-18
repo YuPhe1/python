f2cd67a0475f855b16e4fead52bf2faa0ecbfd6b
YuPhe1 <oksunboy1@naver.com>
2023-08-18 13:28:16 +0900

    08_유튜브_스크롤

---
+++ 08_youtube/ex04_scroll.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# 크롬 브라우저 실행
+driver = webdriver.Chrome()
+# 접속할 주소
+driver.get("https://www.youtube.com/")
+
+driver.maximize_window()    # 창 크기 최대로
+
+# while문을 적용하여 무한스크롤 구현하기
+# while문 내부동작
+# 1. 처음 높이값 확인.
+# 2. 높이 만큼 스크롤 내리기
+# 3. 높이값 확인
+# 4. 높이가 같나면 break로 while문 중단
+while True:
+    h1 = driver.execute_script("return document.documentElement.scrollHeight")
+    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight)")
+    time.sleep(2)
+    h2 = driver.execute_script("return document.documentElement.scrollHeight")
+    if h1 == h2:
+        print(h1, h2, "종료")
+        break
