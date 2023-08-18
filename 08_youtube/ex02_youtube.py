2d1ff0ce670a4ca5750c9e5f41f2555d6a13d490
YuPhe1 <oksunboy1@naver.com>
2023-08-18 10:48:19 +0900

    08_유튜브_크롤링

--- 08_youtube/ex02_youtube.py
+++ 08_youtube/ex02_youtube.py
@@ -3,25 +3,35 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 # 크롬 브라우저 실행
 driver = webdriver.Chrome()
 # 접속할 주소
-driver.get("https://www.youtube.com/")
+# driver.get("https://www.youtube.com/")
 
-driver.maximize_window()
+# driver.maximize_window()
 
 wait = WebDriverWait(driver, 20)
-element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input#search')))
+# element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input#search')))
 
-search = driver.find_element(By.CSS_SELECTOR, 'input#search')
-search.send_keys("뉴진스")
+# search = driver.find_element(By.CSS_SELECTOR, 'input#search')
+# search.send_keys("뉴진스")
 
-search_btn = driver.find_element(By.ID, 'search-icon-legacy')
-search_btn.click()
+# # 엔터 입력
+# search.send_keys(Keys.RETURN)
+
+# search_btn = driver.find_element(By.CSS_SELECTOR, 'button#search-icon-legacy')
+# search_btn.click()
+
+# 검색결과 페이지에 바로 접속
+driver.get("https://www.youtube.com/results?search_query=뉴진스")
 
 element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="video-title"]/yt-formatted-string')))
 titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]/yt-formatted-string')
 for title in titles:
-    print(title.get_attribute("aria-label"))
+    print(title.text)
 time.sleep(10)
+
+# https://www.youtube.com/results?search_query=뉴진스
+# https://www.youtube.com/results?search_query=아이브
---
+++ 08_youtube/ex03_youtube.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+query = input("검색> ")
+url = "https://www.youtube.com/results?search_query=" + query
+
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+wait = WebDriverWait(driver, 20)
+element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="video-title"]/yt-formatted-string')))
+titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]/yt-formatted-string')
+for title in titles:
+    print(title.text)
+time.sleep(10)
