4c2d91c1129fb483cd5595824a7bd33e2f42a11d
YuPhe1 <oksunboy1@naver.com>
2023-08-18 10:58:27 +0900

    08_유튜브_크롤링

--- 08_youtube/ex02_2_youtube.py
+++ 08_youtube/ex02_2_youtube.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
+# 콘솔에서 입력받아 검색
 query = input("검색> ")
+query.replace(" ", "+")
 url = "https://www.youtube.com/results?search_query=" + query
 
 driver = webdriver.Chrome()
