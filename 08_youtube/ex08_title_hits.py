4ab1cc320f77175c4e3002a00d9cb70b1a556f65
YuPhe1 <oksunboy1@naver.com>
2023-08-21 09:10:36 +0900

    08_유튜브_크롤링

--- 08_youtube/ex08_title_hits.py
+++ 08_youtube/ex08_title_hits.py
@@ -5,9 +5,9 @@ import time
 # 크롬 브라우저 실행
 driver = webdriver.Chrome()
 # 접속할 주소
-driver.get("https://www.youtube.com/")
+driver.get("https://www.youtube.com/feed/trending")
 
-time.sleep(5)
+time.sleep(3)
 
 # # 제목요소 가져오기
 # titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
@@ -21,18 +21,32 @@ time.sleep(5)
 #         print("제목:",title.text)
 #         print("조회수:",hits)
 
+# 제목 저장을 위한 리스트
+title_list = []
+# 조회수 저장을 위한 리스트
+hits_list = []
+
 # 제목요소 가져오기
-titles = driver.find_elements(By.XPATH, '//*[@id="video-title-link"]')
+titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
 for title in titles:
-    if title.get_attribute("aria-label"): # shorts 영상을 걸러내기 위한 조건문
+    if title.get_attribute("aria-label") and title.text: # shorts 영상을 걸러내기 위한 조건문
         aria_label = title.get_attribute("aria-label")
         start_index = aria_label.rfind("조회수")+4
         end_index = aria_label.rfind("회")
         if(start_index > 3): # 유튜브 음악 추천 거르기
             hits = aria_label[start_index:end_index]
             hits = int(hits.replace(",",""))
-            print("제목:",title.get_attribute("title"))
-            print("조회수:",hits)
+            # print("제목:",title.get_attribute("title"))
+            # print("조회수:",hits)
+            # 제목, 조회수를 각각 리스트에 담김
+            # append(): 리스트에 데이터를 추가할 떼
+            title_list.append(title.get_attribute("title"))
+            hits_list.append(hits)
 
+# 리스트 데이터 확인
+# print("제목 리스트", title_list)
+# print("조회수 리스트", hits_list)
 
-time.sleep(10)
+# 제목, 조회수 리스트 함께 조회
+for title, hit in zip(title_list, hits_list):
+    print(title, hit)
